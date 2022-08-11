c0187001ec64f410a56a99f6813f59e98f9558a5
Guido Gröön <guido@raulwalter.com>
2022-08-11 16:50:05 +0300

    WE2-689 Updated example

--- examples/src/Pages.php
+++ examples/src/Pages.php
@@ -17,12 +17,6 @@ class Pages
 
     public function welcome()
     {
-        if (!isset($_SESSION["auth-user"])) {
-            // Redirect to login
-            header("location:/");
-            return;
-        }
-
         $data = [];
         $data["auth_user"] = $_SESSION["auth-user"];
         $this->data['content'] = $this->template->getHtml(__DIR__ . '/../tpl/welcome.phtml', $data);
