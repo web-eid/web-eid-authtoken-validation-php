a5ce9539bbaea895d898e72f5cdcfebc6f938cfe
Muzosh <30979983+Muzosh@users.noreply.github.com>
2023-05-21 08:59:26 +0200

    Use internal values for test

--- tests/validator/AuthTokenSignatureValidatorTest.php
+++ tests/validator/AuthTokenSignatureValidatorTest.php
@@ -32,6 +32,7 @@ use web_eid\web_eid_authtoken_validation_php\authtoken\WebEidAuthToken;
 use web_eid\web_eid_authtoken_validation_php\testutil\AbstractTestWithValidator;
 use phpseclib3\Crypt\EC;
 use phpseclib3\File\X509;
+use web_eid\web_eid_authtoken_validation_php\util\AsnUtil;
 
 class AuthTokenSignatureValidatorTest extends TestCase
 {
@@ -68,17 +69,15 @@ class AuthTokenSignatureValidatorTest extends TestCase
     public function testESSignatureDecoding(): void
     {
         $this->expectNotToPerformAssertions();
-        $signatureValidator = new AuthTokenSignatureValidator(new Uri("https://localhost:8443"));
+        $signatureValidator = new AuthTokenSignatureValidator(new Uri("https://ria.ee"));
 
-        $algorithm = 'ES384';
-        $challengeNonce = 'aOakS4V5YCoHfCDhOoj+dr0wr+fQDZMBfWqG7lrM9vA=';
-        $nonAsn1Signature = "vQRLdiycmxodrzxpG2A6SYd8y/vA1ZaxnP3z99ZY5HybPzwGkjyA0vWJMB1UVXOZfyoPlxFPte2xdinsC8/rLbqt2fQbTCfL5WwqIpYN/aI7M7c2wK3NcQtIoy73xCfk";
-        $asn1Signature = "MGUCMQC9BEt2LJybGh2vPGkbYDpJh3zL+8DVlrGc/fP31ljkfJs/PAaSPIDS9YkwHVRVc5kCMH8qD5cRT7XtsXYp7AvP6y26rdn0G0wny+VsKiKWDf2iOzO3NsCtzXELSKMu98Qn5A==";
-        $publicKeyPEM = "-----BEGIN PUBLIC KEY-----\nMHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEMeK51XgGgH291oz/mjO1dKNEgIXwoWEXGiCHb4SpAS28M7x3x1MEvTeig2kTgZsZ5Z7rwlQ2jCownZ7FzL0xnJbWWKf5xvLEejwO8iLkBmBBIOe4Ro9kJ638XyawNOoE\n-----END PUBLIC KEY-----";
+        $authToken = new WebEidAuthToken(AbstractTestWithValidator::VALID_AUTH_TOKEN);
+        $x509Certificate = new X509();
+        $x509Certificate->loadX509($authToken->getUnverifiedCertificate());
 
-        $publicKey = EC::loadFormat('PKCS8', $publicKeyPEM);
+        $asn1Signature = base64_encode(AsnUtil::transcodeSignatureToDER(base64_decode($authToken->getSignature())));
 
-        $signatureValidator->validate($algorithm, $nonAsn1Signature, $publicKey, $challengeNonce);
-        $signatureValidator->validate($algorithm, $asn1Signature, $publicKey, $challengeNonce);
+        $signatureValidator->validate("ES384", $authToken->getSignature(), $x509Certificate->getPublicKey(), AbstractTestWithValidator::VALID_CHALLENGE_NONCE);
+        $signatureValidator->validate("ES384", $asn1Signature, $x509Certificate->getPublicKey(), AbstractTestWithValidator::VALID_CHALLENGE_NONCE);
     }
 }
