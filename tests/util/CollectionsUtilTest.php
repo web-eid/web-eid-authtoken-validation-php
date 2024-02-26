439e83f773655df996a740fafa046181c06a7ba3
Raul Metsma <raul@metsma.ee>
2024-02-26 14:09:07 +0200

    Update copyright year
    WE2-878
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- tests/util/CollectionsUtilTest.php
+++ tests/util/CollectionsUtilTest.php
@@ -1,6 +1,7 @@
 <?php
+
 /*
- * Copyright (c) 2022-2023 Estonian Information System Authority
+ * Copyright (c) 2022-2024 Estonian Information System Authority
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
